d9f3f9e9eeedf38372c5dafb3cebd7059986ab5a
helloworld753315 <helloworld.cpp.h@gmail.com>
2023-11-18 18:29:48 +0900

    ADD TimeTable クラスをapp.pyから呼び出し
    全体の処理をapp.pyでまとめて行うため。

---
+++ app/app.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import yaml
+from modules.excel_converter import TimeTable
+
+
+def main():
+    with open('config.yml') as file:
+        config = yaml.load(file, Loader=yaml.Loader)
+    timetable_config = config["timetable"]
+    print("================================")
+
+    timetable = TimeTable(
+        import_path=timetable_config["import_path"],
+        column_names=timetable_config["column_names"],
+        skip_rows=timetable_config["skip_rows"],
+        use_cols=timetable_config["use_cols"]
+    )
+    timetable.to_json()
+
+
+if __name__ == "__main__":
+    main()
