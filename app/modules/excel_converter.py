d3f8c6ec7319135a465db7f9c5ab2adbe4b0e050
helloworld753315 <helloworld.cpp.h@gmail.com>
2023-11-18 18:20:49 +0900

    ADD Excelの時間割を扱うTimeTableクラスの作成
    Excelから任意の列をDFで取り出すことで後々扱いやすくするため。

---
+++ app/modules/excel_converter.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import warnings
+
+
+class TimeTable:
+    def __init__(self, import_path, column_names, skip_rows, use_cols):
+        self.import_path = import_path
+        self.column_names = column_names
+        self.skip_rows = skip_rows
+        self.use_cols = use_cols
+
+    pd.set_option('display.width', 1000)
+    pd.set_option('display.max_rows', 20)
+
+    def str_to_int(c):
+        return ord(c) - ord('A')
+
+    def to_json(self):
+        # エクセルのマクロが原因だろうと思われる警告の無視
+        warnings.filterwarnings("ignore", message="Unknown extension is not supported and will be removed")
+
+        use_cols = list(map(TimeTable.str_to_int, self.use_cols))
+        use_colmn_names = [self.column_names[i] for i in use_cols]
+        df = pd.read_excel(self.import_path, sheet_name=0, skiprows=12, usecols=use_cols)
+        df.columns = use_colmn_names
+        print(df)
+
