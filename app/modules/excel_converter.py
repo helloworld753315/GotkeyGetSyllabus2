bd046c94f3c816d9634757086b4eb3478adfca6c
helloworld753315 <helloworld.cpp.h@gmail.com>
2024-03-09 11:05:52 +0900

    ADD エクセルの時間割をjsonに出力する機能の修正
    保守性と実効速度をある程度考慮するため、pandasでデータ取得等をやめてopenpyxlとjsonライブラリで動くようにした。

--- app/modules/excel_converter.py
+++ app/modules/excel_converter.py
@@ -1,27 +1,53 @@
-import pandas as pd
 import warnings
+import openpyxl as excel
+import json
 
 
 class TimeTable:
-    def __init__(self, import_path, column_names, skip_rows, use_cols):
+    def __init__(self, import_path, export_path, column_names, skip_rows, use_cols):
         self.import_path = import_path
+        self.export_path = export_path
         self.column_names = column_names
         self.skip_rows = skip_rows
         self.use_cols = use_cols
 
-    pd.set_option('display.width', 1000)
-    pd.set_option('display.max_rows', 20)
-
     def str_to_int(c):
         return ord(c) - ord('A')
 
-    def load(self):
-        # エクセルのマクロが原因だろうと思われる警告の無視
-        warnings.filterwarnings("ignore", message="Unknown extension is not supported and will be removed")
+    # def load(self):
+    #     # エクセルのマクロが原因だろうと思われる警告の無視
+    #     warnings.filterwarnings("ignore", message="Unknown extension is not supported and will be removed")
 
+    #     use_cols = list(map(TimeTable.str_to_int, self.use_cols))
+    #     use_colmn_names = [self.column_names[i] for i in use_cols]
+    #     df = pd.read_excel(self.import_path, sheet_name=0, skiprows=self.skip_rows, usecols=use_cols)
+    #     df.columns = use_colmn_names
+    #     return df
+    
+    def load(self):
+        book = excel.load_workbook(self.import_path)
+        sheet = book.worksheets[0]
         use_cols = list(map(TimeTable.str_to_int, self.use_cols))
+        print(use_cols)
         use_colmn_names = [self.column_names[i] for i in use_cols]
-        df = pd.read_excel(self.import_path, sheet_name=0, skiprows=self.skip_rows, usecols=use_cols)
-        df.columns = use_colmn_names
-        print(df)
+        print(use_colmn_names)
+
+        timetable_list = []
+        for row in range(1 + self.skip_rows, sheet.max_row + 1):
+            # row_data = [sheet[f'{col}{row}'].value for col in self.use_cols]
+            class_details = {}
+            for col in self.use_cols:
+                value_in_cell = sheet[f'{col}{row}'].value
+                use_col = TimeTable.str_to_int(col)
+                class_details[f'{self.column_names[use_col]}'] = value_in_cell
+
+            timetable_list.append(class_details)
+        return timetable_list
+
+    def export_json(self):
+        timetable_list = self.load()
+        with open(self.export_path, 'w') as json_file:
+            json.dump(timetable_list, json_file, indent=4, ensure_ascii=False)
+
+
 
