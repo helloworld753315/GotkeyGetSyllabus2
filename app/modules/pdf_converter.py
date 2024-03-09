8924781d83c7881de666c1f65858a6f9a42a5337
helloworld753315 <helloworld.cpp.h@gmail.com>
2024-03-09 11:49:39 +0900

    UPDATE シラバスの取得方法の修正
    座標を指定してテキストを取得する必要があるため、利便性のためにcsvから座標を設定できるようにした。

--- app/modules/pdf_converter.py
+++ app/modules/pdf_converter.py
@@ -1,44 +1,78 @@
 import fitz
 from pprint import pprint
 import json
+import csv
+import math
+
 
 
 class Syllabus:
     # def load():
-    #     with open('files/com_humanculture2023.pdf', 'rb') as file:
-    #         reader = pypdf.PdfReader(file)
+    #     print("----------------------------------------------------------------")
+    #     doc = fitz.open('files/com_humanculture2023.pdf')
+    #     page = doc[0]  # first page
 
-    #         pdf_pages = reader.pages
+    #     text_info = page.get_text("dict", sort=True)
+    #     # テキストブロックと座標情報の取得
+    #     for block in text_info["blocks"]:
+    #         if block["type"] == 0:  # テキストブロックを意味する
+    #             bbox = block["bbox"]  # ブロックの座標情報
+    #             for line in block["lines"]:
+    #                 for span in line["spans"]:
+    #                     text = span["text"]  # テキスト内容
+    #                     # print(f"Text: {text}, BBox: {bbox}")
+    #                     if int(bbox[0]) == 31 and int(bbox[1]) == 44:
+    #                         print(f"Text: 科目名 {text}, BBox: {bbox}")
 
-    #         number_of_pages = len(pdf_pages)
-    #         print(f"Pages: {number_of_pages}")
-    #         for page_number, page in enumerate(reader.pages):
-    #             print(f"--- Page {page_number + 1} ---")
-    #             text_objects = page.extract_text("rawdict")['blocks']
-    #             for obj in text_objects:
-    #                 if obj['type'] == 0:  # Text object
-    #                     x = obj['bbox'][0]
-    #                     y = obj['bbox'][1]
-    #                     text = obj['text']
-    #                     print(f"Text: {text} (x: {x}, y: {y})")
+    def import_bbox(path='bbox.csv'):
+        with open(path) as f:
+            reader = csv.reader(f)
+            return list(reader)
+
     def load():
+        file_type = 'pdf'
+        path = f'files/com_humanculture2023.{file_type}'
         print("----------------------------------------------------------------")
-        doc = fitz.open('files/com_humanculture2023.pdf')
-        page = doc[0]  # first page
+        pdf_path = 'files/com_humanculture2023.pdf'
+        
+        doc = fitz.open(pdf_path)
 
-        text_info = page.get_text("dict", sort=True)
-        # テキストブロックと座標情報の取得
-        for block in text_info["blocks"]:
-            if block["type"] == 0:  # テキストブロックを意味する
-                bbox = block["bbox"]  # ブロックの座標情報
-                for line in block["lines"]:
-                    for span in line["spans"]:
-                        text = span["text"]  # テキスト内容
-                        # print(f"Text: {text}, BBox: {bbox}")
-                        if int(bbox[0]) == 31 and int(bbox[1]) == 44:
-                            print(f"Text: 科目名 {text}, BBox: {bbox}")
+        number_of_pages = doc.page_count
+        print(f'pages: {number_of_pages}')
 
-        # dir = "files/output.json"
-        # with open(dir, mode="w", encoding="utf-8") as f:
-        #     json.dump(words, f, ensure_ascii=False, indent=2)
-        # pprint(words)
+        page = doc[1]
+        text_objects = page.get_text("dict")["blocks"]
+
+        text_objects_debug = text_objects[:5]
+
+        text_field_bounds = Syllabus.import_bbox()
+
+        syllabus_list = []
+        syllabus_info = {}
+
+        for obj in text_objects:
+            bbox = list(map(float, obj['bbox']))
+            text = obj['lines'][0]['spans'][0]['text']
+        
+            for item in text_field_bounds[1:]:
+                param, x, y = item[0], float(item[1]), float(item[2])
+                
+                if bbox[0] == x and bbox[1] == y:
+                    syllabus_info.setdefault(param, text)
+                    print(text)
+        print(syllabus_info)
+
+
+
+            
+
+        # for obj in text_objects_debug:
+        #     pprint(obj)
+
+        # json_path = 'files/com_humanculture2023.jsonl' 
+
+        # with open(json_path, mode='w', encoding='utf-8') as fout:
+        #     for obj in text_objects:
+        #         pprint(obj)
+        #         json.dump(obj, fout, ensure_ascii=False)
+        #         fout.write('\n')
