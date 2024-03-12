aa03be450911d97ff1026fb3328f7db4adea98fa
helloworld753315 <helloworld.cpp.h@gmail.com>
2024-03-12 21:37:21 +0900

    UPDATE シラバスの読み込みをapp.pyに追加

--- app/app.py
+++ app/app.py
@@ -1,22 +1,38 @@
 import pandas as pd
 import yaml
-from modules.excel_converter import TimeTable
+from modules.timetable import TimeTable
+from modules.syllabus import Syllabus
+import time
 
 
 def main():
     with open('config.yml') as file:
         config = yaml.load(file, Loader=yaml.Loader)
     timetable_config = config["timetable"]
+    syllabus_config = config["syllabus"]
+
     print("================================")
 
     timetable = TimeTable(
         import_path=timetable_config["import_path"],
+        export_path=timetable_config["export_path"],
         column_names=timetable_config["column_names"],
         skip_rows=timetable_config["skip_rows"],
         use_cols=timetable_config["use_cols"]
     )
-    timetable.load()
+    timetable.export_json()
 
+    start = time.time()
+    syllabus = Syllabus(
+        import_path=syllabus_config["import_path"],
+        export_path=syllabus_config["export_path"],
+        text_bbox_setting = syllabus_config["text_bbox_setting"]
+    )
+    syllabus.load()
+    end = time.time()
+    time_diff = end - start
+    print(f'run_time: {time_diff} s')
+    
 
 if __name__ == "__main__":
     main()
