18ca3b4f3a814f9aa624569c08505c79bfc4cbe4
helloworld753315 <helloworld.cpp.h@gmail.com>
2024-03-25 22:43:19 +0900

    CLEAN 不要な関数の削除

--- app/modules/syllabus.py
+++ app/modules/syllabus.py
@@ -30,43 +30,6 @@ class Syllabus:
             reader = reader = csv.DictReader(f, skipinitialspace=True)
             return list(reader)
 
-    def load(self):
-        print("----------------------------------------------------------------")
-        
-        doc = fitz.open(self.import_path)
-
-        number_of_pages = doc.page_count
-        print(f'pages: {number_of_pages}')
-
-        page = doc[0]
-        text_blocks = page.get_text("dict")["blocks"]
-
-        # ---------後で消す--------
-        # with open('tmp/com_humanculture2023_1.json', 'w') as json_file:
-        #     json.dump(text_blocks, json_file, indent=4, ensure_ascii=False)
-
-        text_field_bounds = self.import_bbox()
-
-        syllabus_list = []
-        syllabus_info = {}
-
-        for obj in text_blocks:
-            lines = obj['lines']
-            for line in lines:
-                bbox = list(map(float, line['bbox']))
-                text = line['spans'][0]['text']
-                for item in text_field_bounds[1:]:
-                    param, x, y = item[0], float(item[1]), float(item[2])
-                    
-                    if bbox[0] == x and bbox[1] == y:
-                        syllabus_info.setdefault(param, text)
-                        print(text)
-                    # else:
-                    #     print(f'設定座標 x: {x}, y: {y}')
-                    #     print(f'PDFの座標 x: {bbox[0]}, y: {bbox[1]}')
-                    #     print(f'テキスト: {text}')
-                    print(syllabus_info)
-
     def check_multipage(self, page, x1=282.7799987792969, y1=815.6806030273438, x2=304.7400207519531, y2=824.6806030273438):
         rect = fitz.Rect(x1, y1, x2, y2)
         target_text = page.get_textbox(rect).replace('\n', '')
@@ -145,53 +108,3 @@ class Syllabus:
         
         # print(f'授業回数の一致: {count}')
         # print(f'複数ページのパターン数: {multipage_count}')
-
-
-    def _load(self):
-        print("----------------------------------------------------------------")
-        
-        doc = fitz.open(self.import_path)
-
-        number_of_pages = doc.page_count
-        print(f'pages: {number_of_pages}')
-
-        # ---------後で消す--------
-        # with open('tmp/com_humanculture2023_1.json', 'w') as json_file:
-        #     json.dump(text_blocks, json_file, indent=4, ensure_ascii=False)
-
-        text_field_bounds = self.import_bbox()
-
-        print(text_field_bounds)
-
-        syllabus_list = []
-
-        field_bounds_dict = {(float(item[1]), float(item[2])): item[0] for item in text_field_bounds[1:]}
-
-        for count, page in enumerate(doc):
-            syllabus_info = {}
-            # page = doc[0]
-
-            text_blocks = page.get_text("dict")["blocks"]
-
-            with open('tmp/com_humanculture2023_1.json', 'w') as json_file:
-                json.dump(doc[0].get_text("dict")["blocks"], json_file, indent=4, ensure_ascii=False)
-
-            for obj in text_blocks:
-                lines = obj['lines']
-                for line in lines:
-                    bbox = tuple(map(float, line['bbox'][:2])) # (x, y)のタプルとして格納
-                    text = line['spans'][0]['text']
-                    
-                    # 辞書から直接キーを検索
-                    if bbox in field_bounds_dict:
-                        param = field_bounds_dict[bbox]
-                        syllabus_info.setdefault(param, text)
-            if len(syllabus_info) == len(list(field_bounds_dict)):
-                syllabus_list.append(syllabus_info)
-            else:
-                print(f'failure page: {count}')
-
-        print(f'{len(syllabus_list)}')
-        
-        with open('tmp/com_humanculture2023_test1.json', 'w') as json_file:
-            json.dump(syllabus_list, json_file, indent=4, ensure_ascii=False)
